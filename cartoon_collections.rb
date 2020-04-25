a7298a71552ca8526cb8560350836632ff6f28ae
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-25 02:43:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,3 +21,12 @@ end
 long_planeteer_calls(planeteer_calls)
 
 
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
+end
+
+find_the_cheese(potentially_cheesy_items)
