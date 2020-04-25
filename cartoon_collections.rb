0e5de465bdadb652ec40f03c3369306fa2f7d718
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-04-25 02:43:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,10 @@ end
 summon_captain_planet(planeteer_calls)
 
 
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
+
+long_planeteer_calls(planeteer_calls)
+
+
